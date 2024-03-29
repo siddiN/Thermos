b34efd35473fa40e39c49b79431261f4169476dd
SiddiN <siddinalgonda10@gmail.com>
2024-03-29 13:02:23 +0530

    commit first

---
+++ githubproject/src/githubproject/thermos.java
@@ -0,0 +1,36 @@
+package githubproject;
+
+public class thermos {
+	
+	    private double availableVolume;
+	    private String brand;
+	    private String liquid;
+
+	    // Constructor to initialize the thermos
+	    public thermos() {
+	        availableVolume = 0; // Initially empty
+	        brand = "Godrej";
+	        liquid = "";
+	    }
+
+	    // Method to add liquid into thermos
+	    public void addLiquid(double volume, String liquidType) {
+	        if (availableVolume + volume <= 1.0) { // Capacity of 1 litre
+	            availableVolume += volume;
+	            liquid = liquidType;
+	            System.out.println(volume + " litres of " + liquidType + " added to the thermos.");
+	        } else {
+	            System.out.println("Thermos is full. Cannot add more liquid.");
+	        }
+	    }
+
+	    // Method to display thermos details
+	    public void displayDetails() {
+	        System.out.println("Brand: " + brand);
+	        System.out.println("Available Volume: " + availableVolume + " litres");
+	        System.out.println("Liquid: " + liquid);
+	    }
+	}
+
+
+
